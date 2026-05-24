4ce48945c60c02eee3f02a6e33b4fae4761cd295
Victoria <106978058+LarandaC@users.noreply.github.com>
2026-05-24 13:54:40 -0300

    refactor: remove unused ScrollTrigger import from ScrollProgress component

--- src/components/shared/ScrollProgress.tsx
+++ src/components/shared/ScrollProgress.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export function ScrollProgress() {
   const barRef = useRef<HTMLDivElement>(null);
